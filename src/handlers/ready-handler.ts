7bda30d4b14d7c233fd008155f3be6e407ce17e4
Michael Goodwin <mikejg101@icloud.com>
2020-02-09 23:56:16 -0500

    removed unused import and useless await

--- src/handlers/ready-handler.ts
+++ src/handlers/ready-handler.ts
@@ -1,12 +1,11 @@
 import { HeckFireBot } from "../models/heck-fire-bot";
-import moment = require("moment");
 import { addShieldFromStorage } from "../commands/shield";
 
 export const readyEventHandler = (bot: HeckFireBot) => {
   return async () => {
     bot.logger.info(`Logged in as ${bot.user.tag} using ${bot.name}`);
 
-    await (await bot.storage.values()).forEach(async (entry: any) => {
+    (await bot.storage.values()).forEach(async (entry: any) => {
       if (entry.shield) {
         await addShieldFromStorage(bot, entry.shield);
       }
